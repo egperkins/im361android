5dc4497b40f2073abfc9e14192f8d789a7126e95
Elizabeth <egperkins@mail.bradley.edu>
2019-04-07 22:56:42 -0500

    Broken: Issue with TaskSubmitted
    Crash error I cannot find a fix for
    
    Mocks/ other Activities made

--- app/src/main/java/com/lizzie/connectedhome/TaskSubmitted.kt
+++ app/src/main/java/com/lizzie/connectedhome/TaskSubmitted.kt
@@ -1,7 +1,9 @@
 package com.lizzie.connectedhome
 
-import android.os.Bundle
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
 
 
 class TaskSubmitted : AppCompatActivity(){
--- app/src/main/java/com/lizzie/connectedhome/ViewTask.kt
+++ app/src/main/java/com/lizzie/connectedhome/ViewTask.kt
@@ -1,20 +1,21 @@
 package com.lizzie.connectedhome
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+
+class ViewTask : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_viewtask)
 
         val button: Button = findViewById(R.id.button)
 
         button.setOnClickListener{
-            val intent = Intent(this, TaskAssign::class.java)
+            val intent = Intent(this, ViewTask::class.java)
             startActivity(intent)
         }
     }
